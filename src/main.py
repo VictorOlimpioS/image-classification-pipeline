096d7b3b52ad9983ae6aadd8eb2288b9f16fa7b9
Victor OIimpio <victor.olimpio@gmail.com>
2026-06-18 20:22:43 -0300

    feat: build complete data ingestion pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
+import pathlib
 import ingestion
+import random
+import numpy as np
 
-image_path = "data/raw/IMG_01.jpg"
-image = ingestion.ingest_image(image_path)
+
+images = []
+labels = []
+
+
+
+categories = [("data/raw/non_flooded", 0), ("data/raw/flooded", 1)]
+for category_path, label in categories:
+    category_images = pathlib.Path(category_path).glob("*.jpg")
+    for image in category_images:
+        standardized_image = ingestion.standardize_image(str(image))
+        if standardized_image is not None:
+            images.append(standardized_image)
+            labels.append(label)
+
+combined_data = list(zip(images, labels))
+random.shuffle(combined_data)
+images[:], labels[:] = zip(*combined_data)
+images = np.array(images)
+labels = np.array(labels)
+        
